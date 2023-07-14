6dc815b64aa466f3f82b59ca178e06c0f0f9d600
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-07-13 23:16:06 -0300

    FEAT(arqueiro-mago): Criando classe ArqueiroMago

---
+++ src/modules/arqueiro-mago.js
@@ -0,0 +1,19 @@
+import { Personagem } from "./personagem.js";
+import { Arqueiro } from "./arqueiro.js";
+import { Personagem } from "./personagem.js";
+
+export class ArqueiroMago extends Personagem {
+    ladoArqueiro
+    ladoMago
+    tipo = 'ArqueiroMago'
+
+    constructor(nome, level, destreza, elementoMagico, levelMagico, inteligencia) {
+        super(nome, level)
+        this.ladoArqueiro = new Arqueiro(nome, level, destreza)
+        this.ladoMago = new Mago(nome, level, elementoMagico, levelMagico, inteligencia)
+    }
+
+    obterInsignia() {
+        return `${this.ladoArqueiro.obterInsignia()} = ${this.ladoMago.obterInsignia()}`
+    }
+}
