bb72dad86df1450f2ecbb7308b87a1acb207545e
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-07-14 00:44:23 -0300

    FEAT(personagem-view): Adicionando duelo entre personagem

--- src/components/personagem-view.js
+++ src/components/personagem-view.js
@@ -1,7 +1,15 @@
+import { Personagem } from '../modules/personagem.js'
+import { mostrarModal } from './modal.js'
+
 export class PersonagemView {
+  personagens;
+  personagensSelecionados;
+
   constructor(personagens) {
     this.ulPersonagens = document.querySelector("ul#personagens");
     this.personagens = personagens;
+    this.personagensSelecionados = [];
+    this.escutarEventoDuelo();
   }
 
   render() {
@@ -14,11 +22,11 @@ export class PersonagemView {
 
   criaPersonagem = (personagem) => {
     const personagemLI = document.createElement("li");
-    personagemLI.classList.add("personagem", personagem.tipo);
+    personagemLI.classList.add("personagem", personagem.constructor.tipo);
 
-    //const estaSelecionado = this.personagensSelecionados.indexOf(personagem) !== -1 //sintaxe para quando encontra no array
+    const estaSelecionado = this.personagensSelecionados.indexOf(personagem) !== -1 //sintaxe para quando encontra no array
 
-    //if (estaSelecionado) personagemLI.classList.add('selecionado')
+    if (estaSelecionado) personagemLI.classList.add('selecionado')
 
     personagemLI.innerHTML = `
     <div class="container-superior">
@@ -33,14 +41,14 @@ export class PersonagemView {
         <div class="container-imagem">
             <div class="imagem"></div>
             <div class="container-tipo">
-                <h2 class="tipo">${personagem.tipo}</h2>
+                <h2 class="tipo">${personagem.constructor.tipo}</h2>
             </div>
         </div>
         <div class="container-nome">
             <h3 class="nome">${personagem.nome}</h3>
         </div>
         <div class="container-descricao">
-            <p class="descricao"></p>
+            <p class="descricao">${personagem.constructor.descricao}</p>
         </div>
     </div>
     <div class="container-inferior">
@@ -63,7 +71,7 @@ export class PersonagemView {
         this.render()
     }*/
 
-    /*personagemLI.onclick = () => {
+    personagemLI.onclick = () => {
         const jaTem2Selecionados = this.personagensSelecionados.length === 2
         if (!jaTem2Selecionados || estaSelecionado) {
             personagemLI.classList.toggle('selecionado')
@@ -72,36 +80,43 @@ export class PersonagemView {
 
             this.removeSelecao(personagem)
         }
-    }*/
+    }
 
     return personagemLI;
   };
 
-  /*adicionaSelecao = (personagem) => {
-    this.personagensSelecionados.push(personagem)
-    this.render()
-}
+  adicionaSelecao = (personagem) => {
+    this.personagensSelecionados.push(personagem);
+    this.render();
+  };
 
+  removeSelecao = (personagem) => {
+    const indexDoPersonagemNoArray =
+      this.personagensSelecionados.indexOf(personagem);
+    this.personagensSelecionados.splice(indexDoPersonagemNoArray, 1);
+    this.render();
+  };
 
-removeSelecao = (personagem) => {
-    const indexDoPersonagemNoArray = this.personagensSelecionados.indexOf(personagem)
-    this.personagensSelecionados.splice(indexDoPersonagemNoArray, 1)
-    this.render()
-}
+  escutarEventoDuelo() {
+    const botaoDuelar = document.querySelector(".botao-duelar");
 
-escutarEventoDuelo() {
-    const botaoDuelar = document.querySelector('.botao-duelar')
+    botaoDuelar.addEventListener("click", () => {
+      if (this.personagensSelecionados.length < 2)
+        return mostrarModal("Selecione 2 personagens");
 
-    botaoDuelar.addEventListener('click', () => {
-        if (this.personagensSelecionados.length < 2) return mostrarModal('Selecione 2 personagens')
+      const resultadoDuelo = Personagem.verificarVencedor(
+        this.personagensSelecionados[0],
+        this.personagensSelecionados[1]
+      );
 
-        const resultadoDuelo = Personagem.verificarVencedor(this.personagensSelecionados[0], this.personagensSelecionados[1])
+      mostrarModal(resultadoDuelo);
 
-        mostrarModal(resultadoDuelo)
+      this.personagensSelecionados.splice(
+        0,
+        this.personagensSelecionados.length
+      );
 
-        this.personagensSelecionados.splice(0, this.personagensSelecionados.length)
-
-        this.render()
-    })
-}*/
+      this.render();
+    });
+  }
 }
