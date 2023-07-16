6dddec1a10e1c2efc1c4e9add3c28093a2f26d48
Bruno Ribeiro <bruno.oliveira@bandtec.com.br>
2023-07-15 23:58:29 -0300

    FEAT(index): Implementando classe guerreiro

--- src/index.js
+++ src/index.js
@@ -2,14 +2,15 @@ import { PersonagemView } from "./components/personagem-view.js"
 import { Mago } from "./modules/mago.js"
 import { Arqueiro } from "./modules/arqueiro.js"
 import { ArqueiroMago } from "./modules/arqueiro-mago.js"
+import { Guerreiro } from "./modules/guerreiro.js"
 
-
+const guerreiroJorge = new Guerreiro('Jorge', 8)
 const magoAntonio = new Mago('Antonio', 4, 'fogo', 4, 3)
 const magaJulia = new Mago('Julia', 8, 'gelo', 7, 10)
 const arqueiroBruno = new Arqueiro('Bruno', 7, 8)
 const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'ar', 4, 8)
 
-const personagens = [magoAntonio, magaJulia, arqueiroBruno,arqueiroMagoChico]
+const personagens = [magoAntonio, magaJulia, arqueiroBruno, arqueiroMagoChico, guerreiroJorge]
 
 console.log(Personagem.verificarVencedor(arqueiroBruno, magoAntonio));
 console.log(magaJulia(arqueiroBruno, magoAntonio));
